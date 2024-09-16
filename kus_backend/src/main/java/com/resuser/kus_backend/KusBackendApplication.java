a582cac9109783b31d10a19cd307aab07ea88f32
e.syroezhkin <e.syroezhkin@g.nsu.ru>
2024-09-16 14:55:54 +0700

    backend init

---
+++ kus_backend/src/main/java/com/resuser/kus_backend/KusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resuser.kus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KusBackendApplication.class, args);
+	}
+
+}
---
+++ kus_backend/src/test/java/com/resuser/kus_backend/KusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resuser.kus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
