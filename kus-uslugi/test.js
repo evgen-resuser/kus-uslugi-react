84d667a00871023af056c25db4adaf8cdd33560e
e.syroezhkin <e.syroezhkin@g.nsu.ru>
2024-09-16 14:35:29 +0700

    ?

--- kus-uslugi/test.js
+++
@@ -1,9 +0,0 @@
-let person = {
-    name: "unknown",
-    print() {
-        console.log(this.name + " " + this)
-    }
-}
-
-let lena = Object.create(person, {name: {value: "Lena"}});
-lena.print();
